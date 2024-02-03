843bd0bff3cb06b2d586c6b7848d86af32def935
ACM-Anik <anikmojumder5252@gmail.com>
2024-02-03 21:26:09 +0600

    portfolio almost completed

---
+++ js/portfolios.js
@@ -0,0 +1,76 @@
+var portfolio_data = [
+    {
+        id: "Dance-Revolutions-cover",
+        images: [
+            {
+                src: '../images/Dance-Revolutions-cover.png',
+                thumb: '../images/Dance-Revolutions-cover.png',
+                subHtml: '<h4>Dance Revolutions</h4><p>Summer Camp School</p>',
+            },
+            {
+                src: '../images/Dance-Revolutions-cover-images/1.png',
+                thumb: '../images/Dance-Revolutions-cover-images/1.png',
+            },
+            {
+                src: '../images/Dance-Revolutions-cover-images/2.png',
+                thumb: '../images/Dance-Revolutions-cover-images/2.png',
+            },
+            {
+                src: '../images/Dance-Revolutions-cover-images/3.png',
+                thumb: '../images/Dance-Revolutions-cover-images/3.png',
+            },
+            {
+                src: '../images/Dance-Revolutions-cover-images/4.png',
+                thumb: '../images/Dance-Revolutions-cover-images/4.png',
+            }
+        ]
+    },
+    {
+        id: "Epic-Heroes-Emporium-cover",
+        images: [
+            {
+                src: '../images/Epic-Heroes-Emporium-cover.png',
+                thumb: '../images/Epic-Heroes-Emporium-cover.png',
+                subHtml: '<h4>Epic Heroes Emporium</h4><p>Toy shop</p>',
+            },
+            {
+                src: '../images/Epic-Heroes-Emporium-cover-images/1.png',
+                thumb: '../images/Epic-Heroes-Emporium-cover-images/1.png',
+            },
+            {
+                src: '../images/Epic-Heroes-Emporium-cover-images/2.png',
+                thumb: '../images/Epic-Heroes-Emporium-cover-images/2.png',
+            },
+            {
+                src: '../images/Epic-Heroes-Emporium-cover-images/3.png',
+                thumb: '../images/Epic-Heroes-Emporium-cover-images/3.png',
+            },
+            {
+                src: '../images/Epic-Heroes-Emporium-cover-images/4.png',
+                thumb: '../images/Epic-Heroes-Emporium-cover-images/4.png',
+            }
+        ]
+    },
+    {
+        id: "The-Indic-Cuisine-cover",
+        images: [
+            {
+                src: '../images/The-Indic-Cuisine-cover.png',
+                thumb: '../images/The-Indic-Cuisine-cover.png',
+                subHtml: '<h4>The Indic Cuisine</h4><p>Recipe and chefs details</p>',
+            },
+            {
+                src: '../images/The-Indic-Cuisine-cover-images/1.png',
+                thumb: '../images/The-Indic-Cuisine-cover-images/1.png',
+            },
+            {
+                src: '../images/The-Indic-Cuisine-cover-images/2.png',
+                thumb: '../images/The-Indic-Cuisine-cover-images/2.png',
+            },
+            {
+                src: '../images/The-Indic-Cuisine-cover-images/3.png',
+                thumb: '../images/The-Indic-Cuisine-cover-images/3.png',
+            }
+        ]
+    },
+]
